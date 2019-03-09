82514d29007b8cc19fdabfde3be08b4d953f53b3
Pablete1234 <pabloherrerapalacio@gmail.com>
2019-03-09 01:31:45 +0100

    Update to v1.13.2

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -24,7 +24,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class Main extends Thread {
-    public static final String VERSION = "1.13.2-beta8";
+    public static final String VERSION = "1.13.2";
 
     private static final Gson GSON = new GsonBuilder()
             .registerTypeHierarchyAdapter(byte[].class, new ByteArrayToBase64TypeAdapter()).create();
