2ccdf420f9024e298424f8bc31f3d18e0d416df7
Pablete1234 <pabloherrerapalacio@gmail.com>
2019-07-05 21:35:56 +0200

    Update to v1.13.1 beta 31

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -41,7 +41,7 @@ import java.nio.charset.StandardCharsets;
 
 public class Main extends Thread {
 
-    public static final String VERSION = "1.13.11 beta 29";
+    public static final String VERSION = "1.13.11 beta 31";
 
     public static final Gson GSON = new GsonBuilder()
             .setPrettyPrinting()
