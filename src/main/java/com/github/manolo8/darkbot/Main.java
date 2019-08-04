d607a2dd864c931b5286b7c51ae09dd955cb823a
Pablete1234 <pabloherrerapalacio@gmail.com>
2019-08-04 13:56:48 +0200

    Update to v1.13.12

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -42,7 +42,7 @@ import java.util.stream.Stream;
 
 public class Main extends Thread {
 
-    public static final String VERSION = "1.13.12 beta 5";
+    public static final String VERSION = "1.13.12";
 
     public static final Gson GSON = new GsonBuilder()
             .setPrettyPrinting()
