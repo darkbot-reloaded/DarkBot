08282b905dba6557e3a76e7514b511aee7734f61
Pablete1234 <pabloherrerapalacio@gmail.com>
2019-09-29 04:32:35 +0200

    Update to 1.13.15 beta

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -48,7 +48,7 @@ import java.util.stream.Stream;
 
 public class Main extends Thread implements PluginListener {
 
-    public static final String VERSION_STRING = "1.13.14.1";
+    public static final String VERSION_STRING = "1.13.15 beta";
     public static final Version VERSION = new Version(VERSION_STRING);
 
     public static final Gson GSON = new GsonBuilder()
