c6319b39291a9150cc86f46394615fb78e38e959
Pablete1234 <pabloherrerapalacio@gmail.com>
2019-10-10 23:04:24 +0200

    Pull 1.14's ping manager

--- src/main/java/com/github/manolo8/darkbot/core/manager/PingManager.java
+++ src/main/java/com/github/manolo8/darkbot/core/manager/PingManager.java
@@ -4,79 +4,80 @@ import com.github.manolo8.darkbot.core.BotInstaller;
 import com.github.manolo8.darkbot.core.itf.Manager;
 import com.github.manolo8.darkbot.core.objects.swf.VectorInt;
 
+import java.util.Arrays;
+
 import static com.github.manolo8.darkbot.Main.API;
 
 public class PingManager implements Manager {
 
-    private long pingAddress;
-    private VectorInt lastPings;
+    private volatile VectorInt lastPings = null;
 
-    public int ping;
+    public int ping = -1;
+    private int currSize;
 
-    private int currentIndex;
-    private long lastCheck;
-    private long time;
-
-    public PingManager() {
-        this.lastPings = new VectorInt(0);
-    }
+    private long lastCheck = System.currentTimeMillis();
+    private volatile long time;
 
     @Override
     public void install(BotInstaller botInstaller) {
-        botInstaller.invalid.add(value -> pingAddress = 0);
+        botInstaller.invalid.add(value -> reset());
     }
 
     public void tick() {
-        if (pingAddress == 0) {
-
+        if (time == -1) return;
+        if (!updatePing()) {
+            reset();
+            if (System.currentTimeMillis() - time < 5000) return;
+            time = -1;
             searchPingManager();
-
-        } else if (System.currentTimeMillis() - lastCheck >= 2000) {
-            lastPings.update();
-
-            if (currentIndex == lastPings.size)
-                ping += System.currentTimeMillis() - lastCheck;
-            else if (lastPings.size > 0)
-                ping = lastPings.elements[lastPings.size - 1];
-
-            currentIndex = lastPings.size;
-
-            lastCheck = System.currentTimeMillis();
-            if (ping < 0 || ping > 30_000)
-                System.out.println("Weird ping value (outside of 0-20K): " + ping);
         }
     }
 
+    private void reset() {
+        ping = -1;
+        currSize = 0;
+        lastPings = null;
+    }
+
+    private boolean updatePing() {
+        if (lastPings == null || lastPings.address == 0) return false;
+        lastPings.update();
+        if (lastPings.size <= 0) return false;
+
+        if (currSize != lastPings.size) lastCheck = System.currentTimeMillis();
+        else if (System.currentTimeMillis() - lastCheck > 5000) return false;
+
+        this.currSize = lastPings.size;
+
+        int ping = lastPings.elements[lastPings.size - 1];
+        if (ping < -10 || ping > 50_000) return false;
+
+        this.ping = ping;
+        return true;
+    }
+
     private void searchPingManager() {
+        Arrays.stream(API.queryMemoryLong(1000, 10_000))
+                .unordered()
+                .parallel()
+                .filter(val -> API.readMemoryInt(val + 16) == 0)
+                .map(val -> API.readMemoryLong(val + 8)) // Get ping manager address
+                .distinct()
+                .filter(addr -> addr != 0) // Check it being ping manager
+                .filter(addr -> API.readMemoryLong(addr) != 0)
+                .filter(addr -> API.readMemoryLong(addr + 12) != 0)
+                .filter(addr -> API.readMemoryLong(addr + 36) != 0)
+                .map(addr -> API.readMemoryLong(addr + 40)) // Get last pings array
+                .distinct()
+                .filter(addr -> addr != 0) // Check array
+                .filter(addr -> API.readMemoryInt(addr + 64) > 0) // array size > 0
+                .mapToObj(VectorInt::new)
+                .peek(VectorInt::update)
+                .filter(vec -> vec.size > 0)
+                .filter(vec -> Arrays.stream(vec.elements).allMatch(ping -> ping > -1_000 && ping < 50_000))
+                .findAny()
+                .ifPresent(pings -> this.lastPings = pings);
 
-        if (System.currentTimeMillis() - time > 3000) {
-
-            time = System.currentTimeMillis();
-
-            long[] result = API.queryMemoryLong(1000, 10_000);
-
-            for (long value : result) {
-
-                if (API.readMemoryInt(value + 16) != 0)
-                    continue;
-
-                long address = API.readMemoryLong(value + 8);
-
-                if (address == 0)
-                    continue;
-
-                if (API.readMemoryLong(address) == 0)
-                    continue;
-
-                if (API.readMemoryInt(address + 12) != 0 || API.readMemoryInt(address + 36) != 0)
-                    continue;
-
-                address = API.readMemoryLong(address + 40);
-                if (API.readMemoryInt(address + 64) == 0) continue;
-
-                this.lastPings.update(pingAddress = address);
-                break;
-            }
-        }
+        time = System.currentTimeMillis();
     }
 }
