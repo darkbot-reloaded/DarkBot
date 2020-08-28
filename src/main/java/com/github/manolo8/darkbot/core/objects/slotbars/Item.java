5ae1d1ee2ac89f574e1b0fd5539ce0acbe207e91
Punikekk <50504152+Punikekk@users.noreply.github.com>
2020-08-28 19:02:24 +0200

    Add data to slotbar item

--- src/main/java/com/github/manolo8/darkbot/core/objects/slotbars/Item.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/slotbars/Item.java
@@ -5,8 +5,27 @@ import com.github.manolo8.darkbot.core.itf.UpdatableAuto;
 import static com.github.manolo8.darkbot.Main.API;
 
 public class Item extends UpdatableAuto {
-    public boolean selected;
+    // Use it only if isReady() == false
+    public final ItemTimer itemTimer = new ItemTimer();
+
+    public double quantity;
+    public boolean selected, buyable, activatable, available, visible;
     public String id, counterType, actionStyle, iconLootId;
+
+    @Override
+    public void update() {
+        this.buyable     = API.readMemoryBoolean(address + 36);
+        this.activatable = API.readMemoryBoolean(address + 40);
+        this.selected    = API.readMemoryBoolean(address + 44);
+        this.available   = API.readMemoryBoolean(address + 48);
+        this.visible     = API.readMemoryBoolean(address + 52);
+        //this.blocked   = API.readMemoryBoolean(address + 56); // doesnt work
+        this.quantity    = API.readMemoryDouble(address + 128);
+
+        long tempAddr = API.readMemoryLong(address, 88, 40);
+        if (itemTimer.address != tempAddr) this.itemTimer.update(tempAddr);
+        this.itemTimer.update();
+    }
 
     @Override
     public void update(long address) {
@@ -16,12 +35,35 @@ public class Item extends UpdatableAuto {
             this.counterType = API.readMemoryString(address, 72);
             this.actionStyle = API.readMemoryString(address, 80);
             this.iconLootId  = API.readMemoryString(address, 96);
+
+            //this.itemTimer.update(API.readMemoryLong(address, 88, 40)); sometimes keeps old address
         }
         super.update(address);
     }
 
-    @Override
-    public void update() {
-        this.selected    = API.readMemoryBoolean(address + 44);
+    public boolean isReady() {
+        return this.itemTimer.address == 0;
+    }
+
+    //Object doesnt exist on "ready" state
+    public static class ItemTimer extends UpdatableAuto {
+        public double elapsed, startTime, itemDelay, availableIn;
+
+        @Override
+        public void update() {
+            if (address == 0) return;
+
+            this.elapsed = API.readMemoryDouble(address + 72);
+            this.availableIn = API.readMemoryDouble(address + 96);
+        }
+
+        @Override
+        public void update(long address) {
+            this.address = address;
+            if (address == 0) return;
+
+            this.startTime = API.readMemoryDouble(address + 80);
+            this.itemDelay = API.readMemoryDouble(address + 88);
+        }
     }
 }
