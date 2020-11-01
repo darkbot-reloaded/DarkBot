9ff0206f036dad51185be798dab3d3ed7b6e9a89
kekawce <albertark1589@gmail.com>
2020-10-31 19:06:14 -0700

    RefinementAPI

--- src/main/java/eu/darkbot/api/managers/OreTradeAPI.java
+++ src/main/java/eu/darkbot/api/managers/OreTradeAPI.java
@@ -11,7 +11,8 @@ public interface OreTradeAPI extends Gui, API {
 
     /**
      * Sells the specified ore, trade window must be open for this method to work
-     * @param ore the {@code Ore} you want to sell
+     *
+     * @param ore  the {@code Ore} you want to sell
      * @see #showTrade
      * @see #canSellOres
      */
@@ -19,14 +20,16 @@ public interface OreTradeAPI extends Gui, API {
 
     /**
      * Determines if ores can be sold based on if the ore trade window is open or not.
+     *
      * @return true if ore trade window is open, false otherwise
      */
     boolean canSellOres();
 
     /**
      * Will either open or close the ore trade window based on the value of {@code show}
-     * @param show true for showing ore trade window, false for closing ore trade window
-     * @param base the {@code BasePoint} of the ore trader base station
+     *
+     * @param show  true for showing ore trade window, false for closing ore trade window
+     * @param base  the {@code BasePoint} of the ore trader base station
      * @return true if ore trader window has been opened or closed and its animation is done,
      *         false if animation is not done, or no action is needed to be taken
      *         to change the visibility status of the ore trader window
