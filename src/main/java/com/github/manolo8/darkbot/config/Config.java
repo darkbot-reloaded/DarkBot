214dbc2e380d65ef0abb601df61116ab876c123d
pajtor <marek.matava@gmail.com>
2020-11-02 12:19:24 +0100

    fixed J

--- src/main/java/com/github/manolo8/darkbot/config/Config.java
+++ src/main/java/com/github/manolo8/darkbot/config/Config.java
@@ -104,7 +104,7 @@ public class Config {
     public @Option KeyBinds KEY_BINDS = new KeyBinds();
     public static class KeyBinds{
         public @Option Character CONFIGURATION_KEY = 'C';
-        public @Option Character JUMP_KEY = 'j';
+        public @Option Character JUMP_KEY = 'J';
     }
 
     public @Option Collect COLLECT = new Collect();
