93cd2f37aa9ff794e5a42b55c3f025a49c046460
Pablete1234 <pabloherrerapalacio@gmail.com>
2020-11-02 12:11:12 +0100

    Override table render spinners & char editors for performance

--- src/main/java/com/github/manolo8/darkbot/gui/tree/components/InfoTable.java
+++ src/main/java/com/github/manolo8/darkbot/gui/tree/components/InfoTable.java
@@ -150,4 +150,11 @@ public abstract class InfoTable<T extends TableModel, E> extends JTable implemen
         }
     }
 
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        long start = System.currentTimeMillis();
+        super.paintComponent(g);
+        System.out.println(System.currentTimeMillis() - start);
+    }
 }
--- src/main/java/com/github/manolo8/darkbot/gui/utils/table/TableDoubleRenderer.java
+++ src/main/java/com/github/manolo8/darkbot/gui/utils/table/TableDoubleRenderer.java
@@ -1,15 +1,38 @@
 package com.github.manolo8.darkbot.gui.utils.table;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class TableDoubleRenderer extends TableDelegateRenderer<JSpinner> {
 
     public TableDoubleRenderer() {
-        super(new JSpinner());
+        super(new RenderSpinner());
     }
 
     @Override
     protected void setValue(Object value) {
         delegate.setValue(value);
     }
+
+    private static class RenderSpinner extends JSpinner {
+        /**
+         * No-op methods improve performance when using this as a cell renderer, and they are not needed anyways.
+         */
+        //public void validate() {} // JSpinner needs it to render
+        public void invalidate() {}
+        public void revalidate() {}
+        public void repaint(long tm, int x, int y, int width, int height) {}
+        public void repaint(Rectangle r) {}
+        public void repaint() {}
+        public void firePropertyChange(String propertyName, byte oldValue, byte newValue) {}
+        public void firePropertyChange(String propertyName, char oldValue, char newValue) {}
+        public void firePropertyChange(String propertyName, short oldValue, short newValue) {}
+        public void firePropertyChange(String propertyName, int oldValue, int newValue) {}
+        public void firePropertyChange(String propertyName, long oldValue, long newValue) {}
+        public void firePropertyChange(String propertyName, float oldValue, float newValue) {}
+        public void firePropertyChange(String propertyName, double oldValue, double newValue) {}
+        public void firePropertyChange(String propertyName, boolean oldValue, boolean newValue) {}
+        public void firePropertyChange(String propertyName, Object oldValue, Object newValue) {}
+    }
+
 }
