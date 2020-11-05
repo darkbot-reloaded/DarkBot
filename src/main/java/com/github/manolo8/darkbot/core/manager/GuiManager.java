eea0818eb31af83503eaf724735c8de7672aba9f
Anjesh Shrestha <anjeshshrestha@gmail.com>
2020-11-05 09:42:11 -0700

    Added back DarkBot.iml, removed files from gitgnore and sets it to needrefresh to false first

--- src/main/java/com/github/manolo8/darkbot/core/manager/GuiManager.java
+++ src/main/java/com/github/manolo8/darkbot/core/manager/GuiManager.java
@@ -216,12 +216,11 @@ public class GuiManager implements Manager {
 
         HeroManager hero = main.hero;
         if (this.needRefresh && System.currentTimeMillis() - lastRepair > 5_000){
+            this.needRefresh = false;
             if(main.config.MISCELLANEOUS.REFRESH_AFTER_REVIVE ){
                 System.out.println("Triggering refresh: refreshing after death");
                 API.handleRefresh();
-                return false;
             }
-            this.needRefresh = false;
         }
         if (System.currentTimeMillis() - lastRepair < main.config.GENERAL.SAFETY.WAIT_AFTER_REVIVE * 1000) {
             validTime = System.currentTimeMillis();
