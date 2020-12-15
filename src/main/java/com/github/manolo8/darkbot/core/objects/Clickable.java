cc8f4d4c5ec3ecb623dfa694c32c7bf9ee78a2fe
ODAKAB <odakab54@gmail.com>
2020-12-15 03:26:52 +0300

    Fix clickable.enabled was not updating

--- src/main/java/com/github/manolo8/darkbot/core/objects/Clickable.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/Clickable.java
@@ -68,5 +68,6 @@ public class Clickable extends Updatable {
         this.confirm = API.readMemoryLong(address);
         this.radius = defRadius = API.readMemoryInt(address + 40);
         this.priority = defPriority = API.readMemoryInt(address + 44);
+        this.enabled = API.readMemoryBoolean(address, 64, 32);
     }
 }
