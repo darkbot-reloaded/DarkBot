f6c43526328b055104e94eb49cbd120fee9d0872
Pablete1234 <pabloherrerapalacio@gmail.com>
2021-02-06 23:50:42 +0100

    Continue implementing API from darkbot & use DI scheme

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/EscortProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/EscortProxy.java
@@ -2,10 +2,11 @@ package com.github.manolo8.darkbot.core.objects.facades;
 
 import com.github.manolo8.darkbot.core.itf.Updatable;
 import com.github.manolo8.darkbot.core.utils.ByteUtils;
+import eu.darkbot.api.managers.EscortAPI;
 
 import static com.github.manolo8.darkbot.Main.API;
 
-public class EscortProxy extends Updatable {
+public class EscortProxy extends Updatable implements EscortAPI {
     public double time;
     public int keys;
 
@@ -14,5 +15,15 @@ public class EscortProxy extends Updatable {
 
         this.keys = API.readMemoryInt(API.readMemoryLong(data + 88) + 40);
         this.time = API.readMemoryDouble(API.readMemoryLong(data + 72) + 56);
+    }
+
+    @Override
+    public double getTime() {
+        return time;
+    }
+
+    @Override
+    public double getKeys() {
+        return keys;
     }
 }
