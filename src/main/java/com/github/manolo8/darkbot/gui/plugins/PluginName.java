a774f40e69dd037d2ce2e96e356056fab8d9a2b8
Punikekk <macio521123@gmail.com>
2021-03-29 04:42:45 +0200

    changed to heart image.

--- src/main/java/com/github/manolo8/darkbot/gui/plugins/PluginName.java
+++ src/main/java/com/github/manolo8/darkbot/gui/plugins/PluginName.java
@@ -5,14 +5,13 @@ import com.github.manolo8.darkbot.gui.utils.UIUtils;
 import com.github.manolo8.darkbot.utils.SystemUtils;
 import net.miginfocom.swing.MigLayout;
 
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 
 class PluginName extends JPanel {
 
     PluginName(PluginDefinition definition, JComponent updateButton) {
-        super(new MigLayout("", "[]5px[]5px[]5px[]20px[]20px[]"));
+        super(new MigLayout("", "[]5px[]5px[]5px[]20px[]push[]"));
 
         JLabel name = new JLabel(definition.name);
         Font baseFont = name.getFont();
@@ -31,15 +30,16 @@ class PluginName extends JPanel {
         add(by);
         add(author);
 
-        if (definition.donation != null)
-            add(createDonateButton(definition), "hidemode 2");
-
         add(updateButton, "hidemode 2, height 16!");
+
+        if (definition.donation != null)
+            add(createDonateButton(definition));
+
         setOpaque(false);
     }
 
     private Component createDonateButton(PluginDefinition pd) {
-        JButton donateButton = new JButton("Donate " + pd.author, UIUtils.getIcon("donate", 18, 18));
+        JButton donateButton = new JButton("Donate " + pd.author, UIUtils.getIcon("heart"));
         donateButton.addActionListener(c -> SystemUtils.openUrl(pd.donation.toString()));
 
         return donateButton;
