fe760904604e1a4df0a7e201a77982ec4be75a34
Antoci Gabriel <gabriel.antoci00@gmail.com>
2021-04-04 14:24:18 +0300

    Fix runnigTime does not reset correctly.

--- src/main/java/com/github/manolo8/darkbot/core/manager/StatsManager.java
+++ src/main/java/com/github/manolo8/darkbot/core/manager/StatsManager.java
@@ -144,7 +144,6 @@ public class StatsManager implements Manager {
     }
 
     public void resetValues() {
-        started = System.currentTimeMillis();
         runningTime = 1;
         earnedCredits = 0;
         earnedUridium = 0;
