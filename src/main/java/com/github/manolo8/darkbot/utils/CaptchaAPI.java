5373c0c8c08a1b9115ffdffdcc4ebc10774133d6
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-05-26 11:40:50 -0600

    instance is ternary based on file existing

--- src/main/java/com/github/manolo8/darkbot/utils/CaptchaAPI.java
+++ src/main/java/com/github/manolo8/darkbot/utils/CaptchaAPI.java
@@ -1,16 +1,24 @@
 package com.github.manolo8.darkbot.utils;
 
+import java.io.File;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Map;
+
 public interface CaptchaAPI {
     String SOLVER_PATH = "lib/captchasolver.jar";
-    CaptchaAPI INSTANCE;
-
-    static {
-        try {
+    CaptchaAPI INSTANCE = Files.exists(Paths.get(SOLVER_PATH)) ? ReflectionUtils.createInstance("eu.darkbot.captcha.CaptchaSolver", SOLVER_PATH) : null;
+/*
+//Can not initialize in interface
+static{
+    try {
             INSTANCE = ReflectionUtils.createInstance("eu.darkbot.captcha.CaptchaSolver", SOLVER_PATH);
-        } catch (Exception e) {
-            System.out.printLn("No captcha resolver is configured, if you're not trying to use one you can safely ignore this message");
-        }
+    } catch (Exception e) {
+        System.out.println("No captcha resolver is configured, if you're not trying to use one you can safely ignore this message");
     }
+    }
+ */
 
     static CaptchaAPI getInstance() {
         return INSTANCE;
@@ -18,7 +26,7 @@ public interface CaptchaAPI {
 
     /**
      * Create solving captcha parameters for the provided webpage html string.
-     * @param the url this webpage was obtained from
+     * @param url this webpage was obtained from
      * @param webpage as an HTML string
      * @return form parameters to include as captcha response solution
      */
