50316f95f9888ba2b280ab136dde84c4f9a7410e
Marek <65947112+pajtor@users.noreply.github.com>
2021-06-26 06:33:43 +0200

    Update src/main/java/com/github/manolo8/darkbot/utils/DiscordWebhook.java
    Co-authored-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- src/main/java/com/github/manolo8/darkbot/utils/DiscordWebhook.java
+++ src/main/java/com/github/manolo8/darkbot/utils/DiscordWebhook.java
@@ -13,28 +13,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- Example usage:
-
- DiscordWebhook webhook = new DiscordWebhook("https://discordapp.com/api/webhooks/...");
- webhook.setContent("Any message!");
- webhook.setAvatarUrl("https://your.awesome/image.png");
- webhook.setUsername("Custom Usernames!");
- webhook.setTts(true);
- webhook.addEmbed(new DiscordWebhook.EmbedObject()
- .setTitle("Title")
- .setDescription("This is a description")
- .setColor(Color.RED)
- .addField("1st Field", "Inline", true)
- .addField("2nd Field", "Inline", true)
- .addField("3rd Field", "No-Inline", false)
- .setThumbnail("https://kryptongta.com/images/kryptonlogo.png")
- .setFooter("Footer text", "https://kryptongta.com/images/kryptonlogodark.png")
- .setImage("https://kryptongta.com/images/kryptontitle2.png")
- .setAuthor("Author Name", "https://kryptongta.com", "https://kryptongta.com/images/kryptonlogowide.png")
- .setUrl("https://kryptongta.com"));
- webhook.addEmbed(new DiscordWebhook.EmbedObject()
- .setDescription("Just another added embed object!"));
- webhook.execute(); //Handle exception
+    Source: https://gist.github.com/k3kdude/fba6f6b37594eae3d6f9475330733bdb
  */
 public class DiscordWebhook {
 
