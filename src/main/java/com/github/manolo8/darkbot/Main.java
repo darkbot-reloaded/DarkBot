159eafcd7fb3f1482f333c99875bf8825d05d081
Pablete1234 <pabloherrerapalacio@gmail.com>
2021-09-18 21:04:16 +0200

    Add iml to gitignore & bump version

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -57,7 +57,7 @@ import java.util.stream.Stream;
 
 public class Main extends Thread implements PluginListener, BotAPI {
 
-    public static final Version VERSION      = new Version("1.13.17 beta 100 alpha 5");
+    public static final Version VERSION      = new Version("1.13.17 beta 100 alpha 6");
     public static final Object UPDATE_LOCKER = new Object();
     public static final Gson GSON            = new GsonBuilder()
             .setPrettyPrinting()
