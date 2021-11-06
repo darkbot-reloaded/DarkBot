f117e27d840e87321524e940168a8a1afe72f5df
Marek <65947112+pajtor@users.noreply.github.com>
2021-11-06 17:38:49 +0100

    Update src/main/java/com/github/manolo8/darkbot/utils/DiscordWebhook.java
    Co-authored-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- src/main/java/com/github/manolo8/darkbot/utils/DiscordWebhook.java
+++ src/main/java/com/github/manolo8/darkbot/utils/DiscordWebhook.java
@@ -16,11 +16,12 @@ import java.util.List;
  */
 public class DiscordWebhook {
     private static final Gson GSON = new Gson();
-    private final List<EmbedObject> embeds = new ArrayList<>();
+    
     private String content;
     private String username;
     @SerializedName("avatar_url") private String avatarUrl;
     private boolean tts;
+    private final List<EmbedObject> embeds = new ArrayList<>();
 
     public void setContent(String content) {
         this.content = content;
