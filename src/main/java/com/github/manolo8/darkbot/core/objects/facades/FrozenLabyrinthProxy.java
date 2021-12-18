727508aa1470c4c79e87607c219f0067b04735a1
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-12-17 19:27:15 -0700

    read memory for frozen labyrinth

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
@@ -7,15 +7,18 @@ import static com.github.manolo8.darkbot.Main.API;
 
 public class FrozenLabyrinthProxy extends Updatable {
     public double time;
+    public double synkTime;
     public int keys;
     public String labStatus;
+
 
     public void update() {
         long data = API.readMemoryLong(address + 48) & ByteUtils.ATOM_MASK;
 
         this.keys = API.readMemoryInt(API.readMemoryLong(data + 0x68) + 0x28);
         this.time = API.readMemoryDouble(API.readMemoryLong(data + 0x58) + 0x38);
-        this.labStatus = API.readMemoryString(API.readMemoryLong(data + 0x60));
-    }
+        this.labStatus = API.readMemoryString(API.readMemoryLong(data+0x60));
+        //this.synkTime = API.readMemoryDouble(API.readMemoryLong(data + 0x50) + 0x38);
 
+    }
 }
