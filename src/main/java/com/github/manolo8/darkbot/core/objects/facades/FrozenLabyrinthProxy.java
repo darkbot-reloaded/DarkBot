23e7bff06036d6d3f6d71a31f1a76172ae2d6212
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-12-18 05:19:04 -0700

    Implement frozen labyrinth proxy

---
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
@@ -0,0 +1,21 @@
+package com.github.manolo8.darkbot.core.objects.facades;
+
+import com.github.manolo8.darkbot.core.itf.Updatable;
+import com.github.manolo8.darkbot.core.utils.ByteUtils;
+
+import static com.github.manolo8.darkbot.Main.API;
+
+public class FrozenLabyrinthProxy extends Updatable {
+    public double time;
+    public int keys;
+    public String labStatus;
+
+    public void update() {
+        long data = API.readMemoryLong(address + 48) & ByteUtils.ATOM_MASK;
+
+        this.keys = API.readMemoryInt(API.readMemoryLong(data + 0x68) + 0x28);
+        this.time = API.readMemoryDouble(API.readMemoryLong(data + 0x58) + 0x38);
+        this.labStatus = API.readMemoryString(API.readMemoryLong(data+0x60));
+
+    }
+}
