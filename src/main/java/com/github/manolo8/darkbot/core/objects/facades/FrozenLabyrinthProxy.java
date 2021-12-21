397a5706deb2e2764da6ddb783ac5cc86d7ee81d
Anjesh Shrestha <anjeshshrestha@gmail.com>
2021-12-21 09:46:41 -0700

    formatting

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
@@ -18,10 +18,7 @@ public class FrozenLabyrinthProxy extends Updatable {
         this.keys = API.readMemoryInt(API.readMemoryLong(data + 0x68) + 0x28);
         this.time = API.readMemoryDouble(API.readMemoryLong(data + 0x58) + 0x38);
         this.labStatus = API.readMemoryString(API.readMemoryLong(data+0x60));
-
         this.synkMap = API.readMemoryString(API.readMemoryLong(labVoAddr + 0x28));
         this.synkZone = API.readMemoryInt(labVoAddr + 0x20);;
-
-
     }
 }
