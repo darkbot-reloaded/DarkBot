839f576c7b6fbfcbb6669ad9cc24a87bba9b246b
Anjesh Shrestha <anjeshshrestha@gmail.com>
2022-01-07 22:26:59 -0700

    Frozen lab synk map and zone

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/FrozenLabyrinthProxy.java
@@ -9,13 +9,17 @@ public class FrozenLabyrinthProxy extends Updatable {
     public double time;
     public int keys;
     public String labStatus;
+    public String synkMap;
+    public int synkZone;
 
     public void update() {
         long data = API.readMemoryLong(address + 48) & ByteUtils.ATOM_MASK;
+        long labVoAddr = API.readMemoryLong(data + 0x70) & ByteUtils.ATOM_MASK;
 
         this.keys = API.readMemoryInt(API.readMemoryLong(data + 0x68) + 0x28);
         this.time = API.readMemoryDouble(API.readMemoryLong(data + 0x58) + 0x38);
-        this.labStatus = API.readMemoryString(API.readMemoryLong(data + 0x60));
+        this.labStatus = API.readMemoryString(API.readMemoryLong(data+0x60));
+        this.synkMap = API.readMemoryString(API.readMemoryLong(labVoAddr + 0x28));
+        this.synkZone = API.readMemoryInt(labVoAddr + 0x20);;
     }
-
 }
