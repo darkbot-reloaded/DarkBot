ae624d6d65e44fa15e1f32e41da21f9e7059391e
fabio1999ita <fabio1999ita@gmail.com>
2022-02-07 10:37:14 +0100

    add api system

--- src/main/java/com/github/manolo8/darkbot/gui/players/PlayerEditor.java
+++ src/main/java/com/github/manolo8/darkbot/gui/players/PlayerEditor.java
@@ -4,51 +4,76 @@ import com.github.manolo8.darkbot.Main;
 import com.github.manolo8.darkbot.config.ConfigEntity;
 import com.github.manolo8.darkbot.config.PlayerInfo;
 import com.github.manolo8.darkbot.config.PlayerTag;
-import com.github.manolo8.darkbot.config.UnresolvedPlayer;
-import com.github.manolo8.darkbot.gui.components.MainButton;
-import com.github.manolo8.darkbot.gui.nearplayers.NearPlayerEditor;
 import com.github.manolo8.darkbot.gui.utils.Popups;
 import com.github.manolo8.darkbot.gui.utils.SearchField;
-import com.github.manolo8.darkbot.gui.utils.UIUtils;
 import com.github.manolo8.darkbot.utils.I18n;
+import eu.darkbot.api.events.EventHandler;
+import eu.darkbot.api.events.Listener;
+import eu.darkbot.api.game.entities.Player;
+import eu.darkbot.api.managers.EntitiesAPI;
+import eu.darkbot.api.managers.EventBrokerAPI;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
-public class PlayerEditor extends JPanel {
-    private JList<PlayerInfo> playerInfoList;
-    private DefaultListModel<PlayerInfo> playersModel;
-    private SearchField sf;
-    private PlayerTagEditor tagEditor;
+public class PlayerEditor extends JPanel implements Listener {
+    private final JList<PlayerInfo> playerInfoList;
+    private final DefaultListModel<PlayerInfo> playersModel;
+
+    private final PlayerManager playerManager;
+    private final SearchField sf;
+    private final PlayerTagManager tagEditor;
     protected Main main;
-    NearPlayerEditor nearPlayerEditor = new NearPlayerEditor();
+
+    @EventHandler
+    public void onEntityPlayerAdd(EntitiesAPI.EntityCreateEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (playersModel != null && main.config.PLAYER_INFOS != null && main.config.PLAYER_INFOS.values().stream().noneMatch(a -> a.toString().contains(((Player) event.getEntity()).getEntityInfo().getUsername()))) {
+                playersModel.addElement(new PlayerInfo(((Player) event.getEntity()).getEntityInfo().getUsername(), event.getEntity().getId()));
+                System.out.println("added " + ((Player) event.getEntity()).getEntityInfo().getUsername());
+            } else {
+                System.out.println("in list yet " + ((Player) event.getEntity()).getEntityInfo().getUsername());
+            }
+        }
+    }
+
+    @EventHandler
+    public void onEntityPlayerRemove(EntitiesAPI.EntityRemoveEvent event) {
+        if (event.getEntity() instanceof Player) {
+            if (playersModel != null && main.config.PLAYER_INFOS != null && main.config.PLAYER_INFOS.values().stream().noneMatch(a -> a.toString().contains(((Player) event.getEntity()).getEntityInfo().getUsername()))) {
+                Arrays.stream(playersModel.toArray()).filter(a -> a.toString().contains(((Player) event.getEntity()).getEntityInfo().getUsername())).forEach(playersModel::removeElement);
+                System.out.println("removed " + ((Player) event.getEntity()).getEntityInfo().getUsername());
+            } else {
+                System.out.println("in list yet " + ((Player) event.getEntity()).getEntityInfo().getUsername());
+            }
+        }
+    }
 
     public PlayerEditor() {
-        super(new MigLayout("ins 0, gap 0, wrap 5, fill", "[][][][grow][][]", "[][grow,fill]"));
+        super(new MigLayout("ins 0, gap 0, wrap 3, fill", "[][grow][]", "[][grow,fill]"));
 
-        add(new AddPlayer(), "grow");
-        add(new AddId(), "grow");
-        add(new RemovePlayers(), "grow");
+        add(playerManager = new PlayerManager(this), "grow");
         add(sf = new SearchField(this::refreshList), "grow");
-        add(tagEditor = new PlayerTagEditor(this), "grow");
+        add(tagEditor = new PlayerTagManager(this), "grow");
 
         playerInfoList = new JList<>(playersModel = new DefaultListModel<>());
         playerInfoList.setCellRenderer(new PlayerRenderer());
 
         add(new JScrollPane(playerInfoList,
-                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), "span 5, grow");
-        add(nearPlayerEditor, "grow, cell 5 0 1 2"); //cell column row width height
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER), "span, grow");
     }
 
     public void setup(Main main) {
         this.main = main;
-        nearPlayerEditor.setup(main);
 
         tagEditor.setup(main);
+
+        EventBrokerAPI eventBroker = main.pluginAPI.requireAPI(EventBrokerAPI.class);
+        eventBroker.registerListener(this);
 
         refreshList();
         main.config.PLAYER_UPDATED.add(i -> SwingUtilities.invokeLater(this::refreshList));
@@ -134,50 +159,6 @@ public class PlayerEditor extends JPanel {
             playersModel.removeElement(player);
         }
         ConfigEntity.changed();
-    }
-
-    public class AddPlayer extends MainButton {
-        public AddPlayer() {
-            super(UIUtils.getIcon("add"), I18n.get("players.add_player.by_name"));
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String name = JOptionPane.showInputDialog(this, I18n.get("players.add_player.username"),
-                    I18n.get("players.add_player"), JOptionPane.QUESTION_MESSAGE);
-            if (name == null) return;
-            main.config.UNRESOLVED.add(new UnresolvedPlayer(name));
-        }
-    }
-
-    public class AddId extends MainButton {
-        public AddId() {
-            super(UIUtils.getIcon("add"), I18n.get("players.add_player.by_id"));
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String id = JOptionPane.showInputDialog(this, I18n.get("players.add_player.user_id"),
-                    I18n.get("players.add_player"), JOptionPane.QUESTION_MESSAGE);
-            if (id == null) return;
-            try {
-                main.config.UNRESOLVED.add(new UnresolvedPlayer(Integer.parseInt(id.trim())));
-            } catch (NumberFormatException ex) {
-                Popups.showMessageAsync(I18n.get("players.add_player.invalid_id"),
-                        I18n.get("players.add_player.invalid_id.no_number", id), JOptionPane.ERROR_MESSAGE);
-            }
-        }
-    }
-
-    public class RemovePlayers extends MainButton {
-        public RemovePlayers() {
-            super(UIUtils.getIcon("remove"));
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            removePlayers();
-        }
     }
 
 }
