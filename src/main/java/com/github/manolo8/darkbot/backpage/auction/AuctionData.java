7fd93cfd53cf7b37e0bdaf6b69ca8844869a89c8
Anjesh Shrestha <anjeshshrestha@gmail.com>
2022-03-25 15:31:23 -0600

    added day/week item

--- src/main/java/com/github/manolo8/darkbot/backpage/auction/AuctionData.java
+++ src/main/java/com/github/manolo8/darkbot/backpage/auction/AuctionData.java
@@ -5,32 +5,40 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 public class AuctionData {
     private final Map<String, AuctionItems> auctionItems = new LinkedHashMap<>();
-    private final Pattern AUCTION_TABLE = Pattern.compile("<tr class=\"auctionItemRow .+? (itemKey=\"item_hour[\\S\\s]+?)</tr>", Pattern.DOTALL);
+    private final Pattern AUCTION_TABLE = Pattern.compile("<tr class=\"auctionItemRow[\\S\\s]+?itemGroup=[\\S\\s]+?(itemKey=\"item_[\\S\\s]+?)</tr>", Pattern.DOTALL);
 
-    private final String AUCTION_PATTERN = "auction_item_name_col\">\\s+(.+?)\\s+<.*?" +
+    private final String AUCTION_PATTERN_STRING = "itemKey=\"item_[a-zA-Z]+_(.+?)\".*?" +
+            "auction_item_name_col\">\\s+(.+?)\\s+<.*?" +
             "auction_item_type\">\\s+(.+?)\\s+<.*?" +
             "auction_item_highest\" (.+?)>.*?" +
             "auction_item_current\">\\s+(.+?)\\s+<.*?" +
             "auction_item_you\">\\s+(.+?)\\s+<.*?" +
-            "item_hour_\\d+_buyPrice\" value=\"(.+?)\".*?" +
-            "item_hour_\\d+_lootId\" value=\"(.+?)\".*?";
-    private final Pattern AUCTION_PATTERN_HOUR = Pattern.compile("itemKey=\"item_hour_(.+?)\".*?" + AUCTION_PATTERN, Pattern.DOTALL);
-    private final Pattern AUCTION_PATTERN_DAY = Pattern.compile("itemKey=\"item_day_(.+?)\".*?" + AUCTION_PATTERN, Pattern.DOTALL);
-    private final Pattern AUCTION_PATTERN_WEEK = Pattern.compile("itemKey=\"item_week_(.+?)\".*?" + AUCTION_PATTERN, Pattern.DOTALL);
+            "item_[a-zA-Z]+_\\d+_buyPrice\" value=\"(.+?)\".*?" +
+            "item_[a-zA-Z]+_\\d+_lootId\" value=\"(.+?)\".*?";
+    private final Pattern AUCTION_PATTERN = Pattern.compile( AUCTION_PATTERN_STRING, Pattern.DOTALL);
 
     public Map<String, AuctionItems> getAuctionItems() {
         return auctionItems;
     }
+    public Map<String, AuctionItems> getAuctionHourItems() {
+        return auctionItems.entrySet().stream().filter(a-> a.getValue().type == AuctionItems.Type.HOUR).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+    public Map<String, AuctionItems> getAuctionDayItems() {
+        return auctionItems.entrySet().stream().filter(a-> a.getValue().type == AuctionItems.Type.DAY).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+    public Map<String, AuctionItems> getAuctionWeekItems() {
+        return auctionItems.entrySet().stream().filter(a-> a.getValue().type == AuctionItems.Type.WEEK).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
 
     public boolean parse(String page) {
         Matcher m = AUCTION_TABLE.matcher(page);
         while (m.find()) {
-            if (m.group().contains("item_hour")) buildAuctionItems(m.group(), AUCTION_PATTERN_HOUR);
-            //if (m.group(max).contains("item_day")) buildAuctionItems(m.group(max), AUCTION_PATTERN_DAY);
-            //if (m.group(max).contains("item_week")) buildAuctionItems(m.group(max), AUCTION_PATTERN_WEEK);
+            if (m.group().contains("item_")) buildAuctionItems(m.group(), AUCTION_PATTERN);
         }
         return true;
     }
@@ -82,9 +90,9 @@ public class AuctionData {
         } else {
             r.setHighestBidderId(1L);
         }
-        r.setCurrentBid(Long.parseLong(m.group(5).replace(",", "").replace(".", "")));
-        r.setOwnBid(Long.parseLong(m.group(6).replace(",", "").replace(".", "")));
-        r.setInstantBuy(Long.parseLong(m.group(7).replace(",", "").replace(".", "")));
+        r.setCurrentBid(Long.parseLong("0"+m.group(5).replace(",", "").replace(".", "").replace("-", "")));
+        r.setOwnBid(Long.parseLong("0"+m.group(6).replace(",", "").replace(".", "").replace("-", "")));
+        r.setInstantBuy(Long.parseLong("0"+m.group(7).replace(",", "").replace(".", "").replace("-", "")));
         r.setLootId(m.group(8));
 
         return true;
