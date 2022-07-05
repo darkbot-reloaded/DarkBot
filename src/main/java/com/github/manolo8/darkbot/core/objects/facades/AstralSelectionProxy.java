53bc937905894dbbac97e33c0cbb813349e75aa4
Anjesh <anjesh@ualberta.ca>
2022-07-05 11:34:10 -0700

    astral

---
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/AstralSelectionProxy.java
@@ -0,0 +1,10 @@
+package com.github.manolo8.darkbot.core.objects.facades;
+
+import com.github.manolo8.darkbot.core.itf.Updatable;
+
+public class AstralSelectionProxy extends Updatable {
+    @Override
+    public void update() {
+
+    }
+}
