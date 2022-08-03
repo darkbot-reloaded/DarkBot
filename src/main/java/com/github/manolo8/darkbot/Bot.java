3eef134774d510e4b164cfc4417063baf7f57751
fabio1999ita <fabio1999ita@gmail.com>
2022-08-03 13:57:52 +0200

    rework exception and popup

--- src/main/java/com/github/manolo8/darkbot/Bot.java
+++ src/main/java/com/github/manolo8/darkbot/Bot.java
@@ -1,10 +1,11 @@
 package com.github.manolo8.darkbot;
 
 import com.formdev.flatlaf.FlatDarkLaf;
+import com.github.manolo8.darkbot.gui.utils.Popups;
+import com.github.manolo8.darkbot.gui.utils.UIUtils;
 import com.github.manolo8.darkbot.utils.LibSetup;
 import com.github.manolo8.darkbot.utils.LogUtils;
 import com.github.manolo8.darkbot.utils.StartupParams;
-import eu.darkbot.util.Popups;
 
 import javax.swing.*;
 import java.awt.*;
@@ -14,7 +15,9 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.time.Instant;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Bot {
 
@@ -48,6 +51,9 @@ public class Bot {
 
         Path filePath = Paths.get("curr.pid");
         long currentPid = ProcessHandle.current().pid();
+        ProcessHandle processHandle = ProcessHandle.current();
+        ProcessHandle.Info processInfo = processHandle.info();
+        long currentStartTime = processInfo.startInstant().map(Instant::toEpochMilli).orElse(0L);
 
         if (Files.exists(filePath)) {
             List<String> fileContent = Files.readAllLines(filePath, StandardCharsets.UTF_8);
@@ -55,26 +61,63 @@ public class Bot {
             if (!fileContent.isEmpty()) {
                 for (int i = 0; i < fileContent.size(); i++) {
                     try {
-                        if (ProcessHandle.of(Long.parseLong(fileContent.get(i))).isPresent()) {
-                            Files.writeString(filePath, "\n" + currentPid, StandardOpenOption.APPEND);
-                            Popups.showMessageSync("Multiple bots on the same folder", new JOptionPane(
-                                    "You're currently running multiple bot instances from the same folder.\n" +
-                                    "This can cause crash, unexpected problems, and some broken functionality.\n" +
-                                    "Please create a separate folder to run multiple instances.",
-                                    JOptionPane.WARNING_MESSAGE, JOptionPane.DEFAULT_OPTION));
+                        long filePid = 0;
+                        long fileStartTime = 0;
+                        String[] fileSplit = fileContent.get(i).split(" ");
+                        if (fileSplit.length == 2){
+                            filePid = Long.parseLong(fileSplit[0]);
+                            fileStartTime = Long.parseLong(fileSplit[1]);
+                        }
+
+                        ProcessHandle externalProcessHandle = ProcessHandle.of(filePid).orElse(null);
+                        long externalStartTime = 0;
+                        if (externalProcessHandle != null) {
+                            ProcessHandle.Info ExternalProcessInfo = externalProcessHandle.info();
+                            externalStartTime = ExternalProcessInfo.startInstant().map(Instant::toEpochMilli).orElse(0L);
+                        }
+
+                        if (ProcessHandle.of(filePid).isPresent() && externalStartTime == fileStartTime) {
+                            Files.writeString(filePath, "\n" + currentPid + " " + currentStartTime, StandardOpenOption.APPEND);
+
+                            JButton proceed = new JButton("Proceed Anyways", UIUtils.getIcon("diagnostics"));
+                            JButton cancel = new JButton("Cancel");
+                            AtomicInteger result = new AtomicInteger(-1);
+
+                            proceed.addActionListener(a -> {
+                                SwingUtilities.getWindowAncestor(proceed).setVisible(false);
+                                result.set(0);
+                            });
+                            cancel.addActionListener(a -> {
+                                SwingUtilities.getWindowAncestor(cancel).setVisible(false);
+                                result.set(1);
+                            });
+
+                            Popups.of("Multiple bots on the same folder",
+                                            "You're currently running multiple bot instances from the same folder.\n" +
+                                            "This can cause crash, unexpected problems, and some broken functionality.\n" +
+                                            "Please create a separate folder to run multiple instances.",
+                                            JOptionPane.WARNING_MESSAGE)
+                                    .options(new Object[]{proceed, cancel})
+                                    .initialValue(cancel)
+                                    .showOptionSync();
+
+                            if (result.get() == 1) {
+                                System.out.println("Reject multiple bot warning, closing bot");
+                                System.exit(100);
+                            }
                             break;
                         } else {
                             if (i == fileContent.size() - 1)
-                                Files.writeString(filePath, String.valueOf(currentPid), StandardOpenOption.TRUNCATE_EXISTING);
+                                Files.writeString(filePath, currentPid + " " + currentStartTime, StandardOpenOption.TRUNCATE_EXISTING);
                         }
                     } catch (java.lang.NumberFormatException e) {
                         e.printStackTrace();
-                        Files.writeString(filePath, String.valueOf(currentPid), StandardOpenOption.TRUNCATE_EXISTING);
+                        Files.writeString(filePath, currentPid + " " + currentStartTime, StandardOpenOption.TRUNCATE_EXISTING);
                     }
                 }
             }
         } else {
-            Files.writeString(filePath, String.valueOf(currentPid), StandardOpenOption.CREATE);
+            Files.writeString(filePath, currentPid + " " + currentStartTime, StandardOpenOption.CREATE);
         }
     }
 
