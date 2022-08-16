a66382c62ffa7585ea610de943df3be1b163f279
fabio1999ita <fabio1999ita@gmail.com>
2022-08-16 18:04:26 +0200

    move warning method to Main.java for make works lang

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -33,6 +33,7 @@ import com.github.manolo8.darkbot.extensions.util.VerifierChecker;
 import com.github.manolo8.darkbot.extensions.util.Version;
 import com.github.manolo8.darkbot.gui.MainGui;
 import com.github.manolo8.darkbot.gui.utils.Popups;
+import com.github.manolo8.darkbot.gui.utils.UIUtils;
 import com.github.manolo8.darkbot.modules.DisconnectModule;
 import com.github.manolo8.darkbot.modules.DummyModule;
 import com.github.manolo8.darkbot.modules.TemporalModule;
@@ -49,10 +50,19 @@ import eu.darkbot.api.game.other.Lockable;
 import eu.darkbot.api.managers.BotAPI;
 
 import javax.swing.*;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main extends Thread implements PluginListener, BotAPI {
 
@@ -123,6 +133,10 @@ public class Main extends Thread implements PluginListener, BotAPI {
         this.pluginAPI = new DarkBotPluginApiImpl(this);
         // 3: Initialize i18n with the locale from the config
         I18n.init(pluginAPI, config.BOT_SETTINGS.BOT_GUI.LOCALE);
+
+        checkUniqueInstance(params); //method require min java 9
+        checkJavaVersion(params);
+
         // 4: Generate the actual config
         this.configHandler = pluginAPI.requireInstance(ConfigHandler.class);
 
@@ -425,6 +439,83 @@ public class Main extends Thread implements PluginListener, BotAPI {
     @Override
     public Module getModule() {
         return newModule;
+    }
+
+    private static void checkUniqueInstance(StartupParams params) {
+        if (params.has(StartupParams.LaunchArg.NO_WARN)) return;
+
+        Path filePath = Paths.get("curr.pid");
+        long currentPid = ProcessHandle.current().pid();
+        ProcessHandle processHandle = ProcessHandle.current();
+        ProcessHandle.Info processInfo = processHandle.info();
+        long currentStartTime = processInfo.startInstant().map(Instant::toEpochMilli).orElse(0L);
+
+        List<String> fileContent;
+        try {
+            fileContent = Files.exists(filePath) ? Files.readAllLines(filePath, StandardCharsets.UTF_8) : Collections.emptyList();
+            for (String line : fileContent) {
+                try {
+                    long filePid = 0;
+                    long fileStartTime = 0;
+                    String[] fileSplit = line.split(" ");
+                    if (fileSplit.length == 2) {
+                        filePid = Long.parseLong(fileSplit[0]);
+                        fileStartTime = Long.parseLong(fileSplit[1]);
+                    }
+
+                    ProcessHandle externalProcessHandle = ProcessHandle.of(filePid).orElse(null);
+                    long externalStartTime = 0;
+                    if (externalProcessHandle != null) {
+                        ProcessHandle.Info ExternalProcessInfo = externalProcessHandle.info();
+                        externalStartTime = ExternalProcessInfo.startInstant().map(Instant::toEpochMilli).orElse(0L);
+                    }
+
+                    if (externalProcessHandle != null && externalStartTime == fileStartTime) {
+                        JButton proceed = new JButton(I18n.get("start.same_folder_warn.button.proceed"), UIUtils.getIcon("add")); //TODO add warning icon
+                        JButton cancel = new JButton(I18n.get("start.same_folder_warn.button.cancel"));
+                        AtomicInteger result = new AtomicInteger(-1);
+
+                        proceed.addActionListener(a -> {
+                            SwingUtilities.getWindowAncestor(proceed).setVisible(false);
+                            result.set(0);
+                        });
+                        cancel.addActionListener(a -> {
+                            SwingUtilities.getWindowAncestor(cancel).setVisible(false);
+                            result.set(1);
+                        });
+
+                        Popups.of(I18n.get("start.same_folder_warn.title"),
+                                        I18n.get("start.same_folder_warn.content"),
+                                        JOptionPane.WARNING_MESSAGE)
+                                .options(new Object[]{proceed, cancel})
+                                .initialValue(cancel)
+                                .showOptionSync();
+
+                        if (result.get() == 1) {
+                            System.out.println(I18n.get("start.same_folder_warn.reject"));
+                            System.exit(0);
+                        }
+                        break;
+                    }
+                } catch (java.lang.NumberFormatException e) {
+                    e.printStackTrace();
+                }
+            }
+            Files.writeString(filePath, currentPid + " " + currentStartTime, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void checkJavaVersion(StartupParams params) {
+        if (params.has(StartupParams.LaunchArg.NO_WARN)) return;
+        String java = System.getProperty("java.version");
+
+        if (!java.startsWith("11.") && !java.startsWith("17.") && !java.equals("17")) {
+            Popups.showMessageSync(I18n.get("start.old_java_warn_title"), new JOptionPane(
+                    I18n.get("start.old_java_warn_content"),
+                    JOptionPane.WARNING_MESSAGE, JOptionPane.DEFAULT_OPTION));
+        }
     }
 
 }
