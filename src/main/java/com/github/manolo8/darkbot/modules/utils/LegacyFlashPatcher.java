40a0926b96cc7c260e6260eb86abe0bfecc6bbfd
fabio1999ita <fabio1999ita@gmail.com>
2022-08-22 15:56:11 +0200

    roll back to some win call for admin rights

--- src/main/java/com/github/manolo8/darkbot/modules/utils/LegacyFlashPatcher.java
+++ src/main/java/com/github/manolo8/darkbot/modules/utils/LegacyFlashPatcher.java
@@ -7,9 +7,6 @@ import com.github.manolo8.darkbot.gui.utils.Popups;
 import com.github.manolo8.darkbot.utils.FileUtils;
 
 import javax.swing.*;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
@@ -17,11 +14,15 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class LegacyFlashPatcher {
+
+    private static final Path TMP_SCRIPT = Paths.get("FlashPatcher.bat");
 
     private final List<Fixer> FIXERS = Arrays.asList(
             new FlashInstaller(),
@@ -44,7 +45,19 @@ public class LegacyFlashPatcher {
                         JOptionPane.INFORMATION_MESSAGE)
                 .showSync(); //TODO translate text
 
-        for (Fixer fixer : needFixing) fixer.ApplyPatch(); //TODO make loop for force check if sha256 is ok
+        List<String> script = new ArrayList<>();
+        script.add("@echo off");
+        script.add("chcp 65001");
+        for (Fixer fixer : needFixing) script.addAll(fixer.script()); //TODO make loop for force check if sha256 is ok
+
+        try {
+            Files.write(TMP_SCRIPT, script);
+
+            Runtime.getRuntime().exec("powershell start -verb runas './FlashPatcher.bat'").waitFor();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
+        }
     }
 
     protected void cleanupCache() {
@@ -63,38 +76,40 @@ public class LegacyFlashPatcher {
     private interface Fixer {
         boolean needsFix();
 
-        void ApplyPatch();
+        List<String> script();
     }
 
     private static class FlashInstaller implements Fixer {
+
         private static final Path
                 FLASH_DIR = Paths.get(System.getenv("APPDATA"), "DarkBot", "Flash"),
                 FLASH_OCX = FLASH_DIR.resolve("Flash.ocx"),
                 REG_SVR = Paths.get(System.getenv("WINDIR"), "SysWOW64", "regsvr32");
 
-
         public boolean needsFix() {
             return !Files.exists(FLASH_DIR) || !Files.exists(FLASH_OCX);
         }
 
-        public void ApplyPatch() {
+        public List<String> script() {
             FileUtils.ensureDirectoryExists(FLASH_DIR);
 
             try (InputStream in = new URL("https://darkbot.eu/downloads/Flash.ocx").openStream()) {
                 Files.copy(in, FLASH_OCX, StandardCopyOption.REPLACE_EXISTING);
-                Runtime.getRuntime().exec("\"" + REG_SVR.toAbsolutePath() + "\" \"" + FLASH_OCX.toAbsolutePath() + "\"");
+                //need admin rights
+                //Runtime.getRuntime().exec("\"" + REG_SVR.toAbsolutePath() + "\" \"" + FLASH_OCX.toAbsolutePath() + "\"");
             } catch (IOException e) {
                 e.printStackTrace();
             }
+            return Collections.singletonList("\"" + REG_SVR.toAbsolutePath() + "\"  \"" + FLASH_OCX.toAbsolutePath() + "\"");
         }
     }
 
     private static class FlashConfigSetter implements Fixer {
+
         private static final Path
                 FLASH_FOLDER = Paths.get(System.getenv("WINDIR"), "SysWOW64", "Macromed", "Flash"),
                 FLASH_CONFIG = FLASH_FOLDER.resolve("mms.cfg"),
                 TMP_CONFIG = Paths.get("mms.cfg");
-
 
         private static final List<String> CONFIG_CONTENT = Arrays.asList(
                 "EnableAllowList=1",
@@ -118,15 +133,19 @@ public class LegacyFlashPatcher {
             }
         }
 
-        public void ApplyPatch() {
+        public List<String> script() {
             try {
                 Files.write(TMP_CONFIG, CONFIG_CONTENT);
 
-                FileUtils.ensureDirectoryExists(FLASH_FOLDER);
-                Files.move(TMP_CONFIG, FLASH_CONFIG, StandardCopyOption.REPLACE_EXISTING);
+                //need admin rights
+                //FileUtils.ensureDirectoryExists(FLASH_FOLDER);
+                //Files.move(TMP_CONFIG, FLASH_CONFIG, StandardCopyOption.REPLACE_EXISTING);
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            return Arrays.asList(
+                    "mkdir \"" + FLASH_FOLDER.toAbsolutePath() + "\"",
+                    "move \"" + TMP_CONFIG.toAbsolutePath() + "\" \"" + FLASH_CONFIG.toAbsolutePath() + "\"");
         }
     }
 
