b93204420314fed41e8c11595c3a235992db606b
fabio1999ita <fabio1999ita@gmail.com>
2022-12-11 00:48:04 +0100

    add time event

--- src/main/java/com/github/manolo8/darkbot/utils/LogUtils.java
+++ src/main/java/com/github/manolo8/darkbot/utils/LogUtils.java
@@ -2,6 +2,7 @@ package com.github.manolo8.darkbot.utils;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -15,6 +16,7 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
 
 public class LogUtils {
     public static final DateTimeFormatter LOG_DATE = DateTimeFormatter.ofPattern("uuuu/MM/dd HH:mm:ss.SSS");
@@ -26,14 +28,6 @@ public class LogUtils {
         if (!Files.exists(LOG_FOLDER)) createFolder();
         else removeOld();
 
-        /*try {
-            PrintStream output = getLogger();
-            System.setOut(output);
-            System.setErr(output);
-        } catch (FileNotFoundException | UnsupportedEncodingException e) {
-            System.out.println("Failed to redirect logs, file not found:");
-            e.printStackTrace();
-        }*/
         try {
             FileOutputStream fileOut = new FileOutputStream("logs/" + START_TIME + ".log");
 
@@ -150,8 +144,29 @@ public class LogUtils {
 
         @Override
         public void write(byte @NotNull [] b, int off, int len) throws IOException {
-            for (OutputStream out : outputStreams)
+            StackTraceElement[] stack = Thread.currentThread().getStackTrace();
+            String path = null;
+            if (stack.length > 2) {
+                path = stack[2].toString();
+                path = path.replace("com.github.manolo8.darkbot", "db");
+                path = path.replace("eu.darkbot.api", "api");
+            }
+
+            String string = "[" + LocalDateTime.now().format(LOG_DATE)
+                    + (path != null ? " | " + path : "")
+                    + "] ";
+
+            for (byte out : string.getBytes()){
+                outputStreams[0].write(out);
+            }
+
+            for (byte out : string.getBytes()){
+                outputStreams[1].write(out);
+            }
+
+            for (OutputStream out : outputStreams){
                 out.write(b, off, len);
+            }
         }
 
         @Override
