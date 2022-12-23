ee87e42ce3188b58ecd1fa0d6fe37b37e5857d92
Pablo Herrera <pabloherrerapalacio@gmail.com>
2022-12-23 23:57:55 +0100

    Bump to beta 114
    Signed-off-by: Pablo Herrera <pabloherrerapalacio@gmail.com>

--- src/main/java/com/github/manolo8/darkbot/Main.java
+++ src/main/java/com/github/manolo8/darkbot/Main.java
@@ -58,7 +58,7 @@ import java.util.Objects;
 
 public class Main extends Thread implements PluginListener, BotAPI {
 
-    public static final Version VERSION      = new Version("1.13.17 beta 114 alpha 3");
+    public static final Version VERSION      = new Version("1.13.17 beta 114");
     public static final Object UPDATE_LOCKER = new Object();
     public static final Gson GSON            = new GsonBuilder()
             .setPrettyPrinting()
