cfa1c593e37463923054f1c1acd04290d76974cc
Pablo Herrera <pabloherrerapalacio@gmail.com>
2022-12-24 16:47:21 +0100

    Optimize trail generation GC

--- src/main/java/com/github/manolo8/darkbot/gui/drawables/TrailDrawer.java
+++ src/main/java/com/github/manolo8/darkbot/gui/drawables/TrailDrawer.java
@@ -2,6 +2,7 @@ package com.github.manolo8.darkbot.gui.drawables;
 
 import com.github.manolo8.darkbot.config.ColorScheme;
 import com.github.manolo8.darkbot.gui.trail.Line;
+import com.github.manolo8.darkbot.utils.data.RecyclingQueue;
 import eu.darkbot.api.config.ConfigSetting;
 import eu.darkbot.api.extensions.Draw;
 import eu.darkbot.api.extensions.Drawable;
@@ -15,7 +16,6 @@ import eu.darkbot.api.managers.HeroAPI;
 import java.awt.*;
 import java.util.Collection;
 import java.util.List;
-import java.util.TreeMap;
 
 @Feature(name = "Trail Drawer", description = "Draws hero's trail")
 @Draw(value = Draw.Stage.HERO_TRAIL, attach = Draw.Attach.REPLACE)
@@ -23,9 +23,9 @@ public class TrailDrawer implements Drawable {
 
     private final ConfigSetting<Integer> trailLength;
     private final ConfigSetting<ColorScheme> cs;
-    private final TreeMap<Long, Line> positions = new TreeMap<>();
+    private final RecyclingQueue<Line> positions = new RecyclingQueue<>(Line::new);
     private final HeroAPI hero;
-    private Location last = Location.of(0, 0);
+    private final Location last = Location.of(0, 0);
 
     public TrailDrawer(HeroAPI hero, ConfigAPI config) {
         this.hero = hero;
@@ -43,17 +43,23 @@ public class TrailDrawer implements Drawable {
         double distance = last.distanceTo(hero);
 
         if (distance > 500) {
-            last = hero.getLocationInfo().copy();
+            last.setTo(hero);
         } else if (distance > 100) {
-            positions.put(System.currentTimeMillis(), new Line(last, last = hero.getLocationInfo().copy()));
+            positions.add().init(last, last.setTo(hero));
         }
-        positions.headMap(System.currentTimeMillis() - trailLength.getValue() * 1000L).clear();
+
+        long removeBefore = System.currentTimeMillis() - trailLength.getValue() * 1000L;
+        while (!positions.isEmpty()) {
+            Line line = positions.get();
+            if (line.getTime() > removeBefore) break;
+            positions.remove();
+        }
 
         if (positions.isEmpty()) return;
 
         Stroke stroke = mg.getGraphics2D().getStroke();
         mg.getGraphics2D().setStroke(new BasicStroke(1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_ROUND));
-        List<List<Location>> paths = Line.getSmoothedPaths(positions.values());
+        List<List<Location>> paths = Line.getSmoothedPaths(positions);
 
         double max = paths.stream().mapToInt(Collection::size).sum() / 255d, curr = 0;
         for (List<Location> points : paths) {
