d9fc16ce4ac4349e87b844ba99cc817ff3ddee47
anjesh <anjeshshrestha@gmail.com>
2023-01-12 21:39:33 +0100

    add plutus status

--- src/main/java/com/github/manolo8/darkbot/core/manager/FacadeManager.java
+++ src/main/java/com/github/manolo8/darkbot/core/manager/FacadeManager.java
@@ -17,6 +17,7 @@ import com.github.manolo8.darkbot.core.objects.facades.SettingsProxy;
 import com.github.manolo8.darkbot.core.objects.facades.SlotBarsProxy;
 import com.github.manolo8.darkbot.core.objects.facades.SpaceMapWindowProxy;
 import com.github.manolo8.darkbot.core.objects.facades.StatsProxy;
+import com.github.manolo8.darkbot.core.objects.facades.PlutusProxy;
 import com.github.manolo8.darkbot.core.objects.swf.PairArray;
 import eu.darkbot.api.PluginAPI;
 
@@ -47,6 +48,7 @@ public class FacadeManager implements Manager, eu.darkbot.api.API.Singleton {
     public final AstralGateProxy astralGate;
     public final HighlightProxy highlight;
     public final SpaceMapWindowProxy spaceMapWindowProxy;
+    public final PlutusProxy plutusProxy;
 
     public FacadeManager(PluginAPI pluginApi) {
         this.pluginAPI = pluginApi;
@@ -64,7 +66,8 @@ public class FacadeManager implements Manager, eu.darkbot.api.API.Singleton {
         this.chrominEvent   = registerProxy("chrominEvent",           ChrominProxy.class);
         this.astralGate     = registerProxy("rogue_lite",             AstralGateProxy.class);
         this.highlight      = registerProxy("HighlightProxy",         HighlightProxy.class);
-        this.spaceMapWindowProxy = registerProxy("spacemap",     SpaceMapWindowProxy.class);
+        this.spaceMapWindowProxy = registerProxy("spacemap",          SpaceMapWindowProxy.class);
+        this.plutusProxy    = registerProxy("plutus",                 PlutusProxy.class);
     }
 
     public <T extends Updatable> T registerCommand(String key, Class<T> commandClass) {
