2bfb394f40c683b683d66837d8df09b130fe359a
Anjesh Shrestha <anjeshnp@amazon.com>
2023-01-12 13:09:34 -0800

    rename to status

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/PlutusProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/PlutusProxy.java
@@ -6,13 +6,13 @@ import com.github.manolo8.darkbot.core.utils.ByteUtils;
 import static com.github.manolo8.darkbot.Main.API;
 
 public class PlutusProxy extends Updatable implements eu.darkbot.api.API.Singleton {
-    public String platusStatus;
+    public String status;
 
     @Override
     public void update() {
         long data = API.readMemoryLong(address + 48) & ByteUtils.ATOM_MASK;
         long plutusVoAddr = API.readMemoryLong(data + 0x58) & ByteUtils.ATOM_MASK;
 
-        this.platusStatus = API.readMemoryString(API.readMemoryLong(plutusVoAddr+0x30));
+        this.status = API.readMemoryString(API.readMemoryLong(plutusVoAddr+0x30));
     }
 }
