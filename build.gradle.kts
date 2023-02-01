d403e64dd5089c634d3d3ffdd15ddf916abffac4
Pablo Herrera <pabloherrerapalacio@gmail.com>
2023-02-01 21:07:54 +0100

    Switch back to jitpack build

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,8 +46,8 @@ dependencies {
     val apiVersion = "0.7.0"
 
     // use this if you want to use local(mavenLocal) darkbot API
-    api("eu.darkbot", "darkbot-impl", apiVersion)
-    //api("eu.darkbot.DarkBotAPI", "darkbot-impl", apiVersion)
+    //api("eu.darkbot", "darkbot-impl", apiVersion)
+    api("eu.darkbot.DarkBotAPI", "darkbot-impl", apiVersion)
 
     api("com.google.code.gson", "gson", "2.8.9")
     api("com.miglayout", "miglayout", "3.7.4")
