d23ade3b2b8d32863cd4486bc769389826658006
Anjesh Shrestha <anjeshshrestha@gmail.com>
2023-05-11 20:21:54 -0600

    spacing

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/NpcEventProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/NpcEventProxy.java
@@ -23,29 +23,32 @@ public class NpcEventProxy extends Updatable implements NpcEventAPI {
     }
 
     @Override
-    public String getEventName(){
+    public String getEventName() {
         return eventName;
     }
 
     @Override
-    public String getEventID(){
+    public String getEventID() {
         return eventID;
     }
 
     @Override
-    public String getEventDescription(){
+    public String getEventDescription() {
         return eventDescription;
     }
+
     @Override
-    public String getNpcLeftDescription(){
+    public String getNpcLeftDescription() {
         return npcLeftDescription;
     }
+
     @Override
-    public int npcLeft(){
+    public int npcLeft() {
         return npcCount;
     }
+
     @Override
-    public int bossNpcLeft(){
+    public int bossNpcLeft() {
         return bossCount;
     }
 
@@ -61,14 +64,14 @@ public class NpcEventProxy extends Updatable implements NpcEventAPI {
     public void update() {
         long data = API.readMemoryLong(address + 48) & ByteUtils.ATOM_MASK;
 
-        this.eventActive = API.readBoolean(data+0x44);
-        this.time = API.readMemoryDouble(API.readMemoryLong(data+0x50)+0x38);
-        this.eventDescription = API.readMemoryString(API.readMemoryLong(data+0x60));
-        this.npcLeftDescription = API.readMemoryString(API.readMemoryLong(data+0x68));
-        this.eventDescriptionID = API.readMemoryString(API.readMemoryLong(data+0x70));
-        this.eventID = API.readMemoryString(API.readMemoryLong(data+0x78));
-        this.eventName = API.readMemoryString(API.readMemoryLong(data+0x80));
-        this.npcCount = API.readInt(data+0x88);
-        this.bossCount = API.readInt(data+0x90);
+        this.eventActive = API.readBoolean(data + 0x44);
+        this.time = API.readMemoryDouble(API.readMemoryLong(data + 0x50) + 0x38);
+        this.eventDescription = API.readMemoryString(API.readMemoryLong(data + 0x60));
+        this.npcLeftDescription = API.readMemoryString(API.readMemoryLong(data + 0x68));
+        this.eventDescriptionID = API.readMemoryString(API.readMemoryLong(data + 0x70));
+        this.eventID = API.readMemoryString(API.readMemoryLong(data + 0x78));
+        this.eventName = API.readMemoryString(API.readMemoryLong(data + 0x80));
+        this.npcCount = API.readInt(data + 0x88);
+        this.bossCount = API.readInt(data + 0x90);
     }
 }
