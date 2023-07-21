3260ae874d469bba26528439fcf07b6a85f88610
Pablo Herrera <pabloherrerapalacio@gmail.com>
2023-07-21 18:47:40 +0200

    Implement spin api from new GG data

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/GalaxyBuilderProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/GalaxyBuilderProxy.java
@@ -3,80 +3,144 @@ package com.github.manolo8.darkbot.core.objects.facades;
 import com.github.manolo8.darkbot.Main;
 import com.github.manolo8.darkbot.core.itf.Updatable;
 import com.github.manolo8.darkbot.core.objects.swf.ObjArray;
-import com.github.manolo8.darkbot.utils.Time;
-import eu.darkbot.api.API;
 import eu.darkbot.api.game.galaxy.GalaxyGate;
+import eu.darkbot.api.game.galaxy.GalaxyInfo;
+import eu.darkbot.api.game.galaxy.GateInfo;
+import eu.darkbot.api.game.galaxy.SpinResult;
+import eu.darkbot.api.managers.GalaxySpinnerAPI;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.Optional;
 
-public class GalaxyBuilderProxy extends Updatable implements API.Singleton {
+public class GalaxyBuilderProxy extends Updatable implements GalaxySpinnerAPI {
 
-    private final BuilderData builderData = new BuilderData();
+    @Getter
+    private final BuilderData galaxyInfo = new BuilderData();
 
     @Override
     public void update() {
-        this.builderData.update(readAtom(48));
+        this.galaxyInfo.update(readAtom(48));
     }
 
     // 1, 5, 10, 100
     public void spin(GalaxyGate gate, int amount) {
-        if (!builderData.initialized) return;
+        if (!galaxyInfo.initialized) return;
 
-        Main.API.callMethodAsync(19, builderData.address, gate.getId()); // set current gate
-        Main.API.callMethodAsync(70, builderData.address, amount); // set spin amount
-        Main.API.callMethodAsync(68, builderData.address); // make a spin
+        Main.API.callMethodAsync(19, galaxyInfo.address, gate.getId()); // set current gate
+        Main.API.callMethodAsync(70, galaxyInfo.address, amount); // set spin amount
+        Main.API.callMethodAsync(68, galaxyInfo.address); // make a spin
+    }
+
+
+    @Override
+    public @Nullable Boolean updateGalaxyInfos(int expiryTime) {
+        // TODO: implement
+        return true;
     }
 
     @Override
-    public String toString() {
-        return "GalaxyBuilderProxy{" +
-                "builderData=" + builderData +
-                '}';
+    public Optional<SpinResult> spinGate(@NotNull GalaxyGate gate, boolean multiplier, int spinAmount, int minWait) {
+        // TODO: implement
+        return Optional.empty();
     }
 
-    private static class BuilderData extends Updatable.Auto {
+    @Override
+    public int getSpinsUsed() {
+        // TODO: implement
+        return 0;
+    }
+
+    @Override
+    public boolean placeGate(@NotNull GalaxyGate gate, int minWait) {
+        // TODO: implement
+        return false;
+    }
+
+    @Override
+    public boolean buyLife(@NotNull GalaxyGate gate, int minWait) {
+        // TODO: implement
+        return false;
+    }
+
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    private static class BuilderData extends Updatable.Auto implements GalaxyInfo {
         private final ObjArray gatesArr = ObjArray.ofArrObj(true);
-        private final List<GateData> gateData = new ArrayList<>();
-        private int freeSpins, selectedSpinAmount, spinPrice, selectedGateId;
+        private final Map<GalaxyGate, GateInfoImpl> gateData = new EnumMap<>(GalaxyGate.class);
+        private int freeEnergy, selectedSpinAmount, energyCost, selectedGateId;
         private boolean initialized;
 
-        private double uridium;
+        private int uridium;
+
+        public BuilderData() {
+            for (GalaxyGate gate : GalaxyGate.values()) {
+                gateData.put(gate, new GateInfoImpl());
+            }
+        }
 
         @Override
         public void update() {
-            this.freeSpins = readInt(64);
-            this.spinPrice = readInt(68);
+            this.freeEnergy = readInt(64);
+            this.energyCost = readInt(68);
             this.selectedGateId = readInt(76);
 
             this.initialized = readBoolean(80);
             this.selectedSpinAmount = readInt(96);
 
-            this.gatesArr.update(readLong(112));
-            this.gatesArr.sync(gateData, GateData::new);
+            this.uridium = (int) readDouble(288);
 
-            this.uridium = readDouble(288);
+            if (initialized) {
+                this.gatesArr.update(readLong(112));
+                for (GalaxyGate gate : GalaxyGate.values()) {
+                    gateData.get(gate).update(gatesArr.getPtr(gate.getId() - 1));
+                }
+            }
         }
 
         @Override
-        public String toString() {
-            return "BuilderData{" +
-                    "gatesArr=" + gatesArr +
-                    ", gateData=" + gateData +
-                    ", freeSpins=" + freeSpins +
-                    ", selectedSpinAmount=" + selectedSpinAmount +
-                    ", spinPrice=" + spinPrice +
-                    ", selectedGateId=" + selectedGateId +
-                    ", initialized=" + initialized +
-                    ", uridium=" + uridium +
-                    '}';
+        public int getSpinSalePercentage() {
+            return 100 - energyCost;
         }
+
+        @Override
+        public boolean isSpinSale() {
+            return energyCost != 100; // TODO: read data from static field in-game
+        }
+
+        @Override
+        public boolean isGalaxyGateDay() {
+            return false; // TODO: read data from static field in-game
+        }
+
+        @Override
+        public boolean isBonusRewardsDay() {
+            return false; // TODO: read data from static field in-game
+        }
+
+        @Override
+        public Map<GalaxyGate, ? extends GateInfo> getGateInfos() {
+            return gateData;
+        }
+
+        @Override
+        public GateInfo getGateInfo(GalaxyGate galaxyGate) {
+            return gateData.get(galaxyGate);
+        }
+
     }
 
-    private static class GateData extends Updatable.Auto {
-        private final BonusReward bonusReward = new BonusReward();
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    private static class GateInfoImpl extends Updatable.Auto implements GateInfo {
+        private final BonusRewardImpl bonusReward = new BonusRewardImpl();
         private int currentParts, totalParts;
-        private int livesLeft, livePrice;
+        private int livesLeft, lifePrice;
         private int currentWave, totalWave;
         private boolean readyToPlace;
 
@@ -87,7 +151,7 @@ public class GalaxyBuilderProxy extends Updatable implements API.Singleton {
             this.totalParts = readInt(36);
 
             this.livesLeft = readInt(40);
-            this.livePrice = readInt(44);
+            this.lifePrice = readInt(44);
 
             this.readyToPlace = readBoolean(48);
 
@@ -98,27 +162,30 @@ public class GalaxyBuilderProxy extends Updatable implements API.Singleton {
         }
 
         @Override
-        public String toString() {
-            return "GateData{" +
-                    "bonusReward=" + bonusReward +
-                    ", currentParts=" + currentParts +
-                    ", totalParts=" + totalParts +
-                    ", livesLeft=" + livesLeft +
-                    ", livePrice=" + livePrice +
-                    ", currentWave=" + currentWave +
-                    ", totalWave=" + totalWave +
-                    ", readyToPlace=" + readyToPlace +
-                    '}';
+        public boolean isOnMap() {
+            return livesLeft > 0;
+        }
+
+        @Override
+        public int getMultiplier() {
+            // TODO: read multiplier
+            return 0;
+        }
+
+        @Override
+        public Optional<BonusReward> getBonusReward() {
+            return bonusReward.valid ? Optional.of(bonusReward) : Optional.empty();
         }
     }
 
-    private static class BonusReward extends Updatable.Auto {
+    @Data
+    @EqualsAndHashCode(callSuper = true)
+    private static class BonusRewardImpl extends Updatable implements GateInfo.BonusReward {
         private String lootId;
-        private int amount, countdown, countdownTimer;
-
+        private int amount, countdown;
         private boolean claimed, valid;
 
-        private long localTime;
+        private long lastUpdate;
 
         @Override
         public void update() {
@@ -135,28 +202,14 @@ public class GalaxyBuilderProxy extends Updatable implements API.Singleton {
             boolean changed = address != this.address;
             super.update(address);
 
-            if (changed) {
+            if (changed && valid) {
                 this.lootId = readString(48);
-                this.localTime = System.currentTimeMillis();
-                //this.countdownTimer = readInt(44);
+                this.lastUpdate = System.currentTimeMillis();
             }
         }
 
-        public String getCountdown() {
-            return Time.secondsToString((int) (countdown - (System.currentTimeMillis() - localTime) / 1000));
-        }
-
-        @Override
-        public String toString() {
-            return "BonusReward{" +
-                    "lootId='" + lootId + '\'' +
-                    ", amount=" + amount +
-                    ", countdown=" + countdown +
-                    ", countdownTimer=" + countdownTimer +
-                    ", claimed=" + claimed +
-                    ", valid=" + valid +
-                    ", localTime=" + localTime +
-                    '}';
+        public int getCountdown() {
+            return (int) (countdown - (System.currentTimeMillis() - lastUpdate) / 1000);
         }
     }
 }
--- src/main/java/com/github/manolo8/darkbot/utils/debug/ReadObjNames.java
+++ src/main/java/com/github/manolo8/darkbot/utils/debug/ReadObjNames.java
@@ -1,9 +1,12 @@
 package com.github.manolo8.darkbot.utils.debug;
 
+import lombok.RequiredArgsConstructor;
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -20,6 +23,24 @@ public class ReadObjNames {
     private static Map<String, String> replacements = new HashMap<>();
     private static long lastModified;
 
+    @RequiredArgsConstructor
+    enum Type {
+        OBJ("\u001B[34m", 8),
+        STR("\u001B[32m", 8),
+        BOOL("\u001B[32m", 4),
+        INT("\u001B[32m", 4),
+        LNG("\u001B[32m", 8),
+        DBL("\u001B[32m", 8);
+
+        private final String ansiColor;
+        private final int size;
+
+        @Override
+        public String toString() {
+            return ansiColor + name();
+        }
+    }
+
     public static void of(long address) {
         of(address, 0x400);
     }
@@ -31,17 +52,17 @@ public class ReadObjNames {
 
         for (int offset = 0; offset < maxOffset; offset++) {
             long addr = API.readMemoryLong(address + offset);
-            //prints string at offset
-            print(addr, offset, false);
+            Type type = null;
 
-            if (isInvalid(addr)) continue;
-            addr = API.readMemoryLong(addr + 0x10);
-            if (isInvalid(addr)) continue;
-            addr = API.readMemoryLong(addr + 0x28);
-            if (isInvalid(addr)) continue;
+            if (!isInvalid(addr)) {
+                type = printObjName(API.readMemoryLong(addr, 0x10, 0x28, 0x90), offset);
+            }
 
-            //prints object name at offset
-            print(API.readMemoryLong(addr + 0x90), offset, true);
+            if (type == null) {
+                type = findType(address, offset);
+            }
+
+            if (type != null) offset += (type.size - 1);
         }
     }
 
@@ -49,15 +70,50 @@ public class ReadObjNames {
         return (address < 0xFF00000000L || address > 0xF000000000000L);
     }
 
-    private static void print(long address, int offset, boolean isObject) {
-        String result = API.readMemoryStringFallback(address, null);
+    private static Type findType(long address, int offset) {
+        if ((offset % 4) != 0) return null;
 
-        if (result != null && !result.isEmpty())
-            System.out.println(formatString(replacements.getOrDefault(result, result), offset, isObject));
+        String strVal = API.readMemoryStringFallback(address + offset, null);
+        if (strVal != null) {
+            printStr( "\"" + strVal + "\"", offset, Type.STR);
+            return Type.STR;
+        }
+
+        long longVal = API.readMemoryLong(address + offset);
+        if (longVal == 0) return null;
+
+        double dblVal = Double.longBitsToDouble(longVal);
+
+        // Probably a double, has a sensible exponent and is non-zero
+        if (Math.abs(Math.getExponent(dblVal)) < 32 && BigDecimal.valueOf(dblVal).scale() < 5) {
+            printStr(Double.toString(dblVal), offset, Type.DBL);
+            return Type.DBL;
+        }
+        // Probably an int in the previous slot, the right-half is all 0s
+        if (((longVal >> 32) << 32) == longVal) {
+            return null;
+        }
+        // Assume it's an int
+        if (Math.abs((int) longVal) <= 0xFFFFFF) {
+            printStr(Integer.toString((int) longVal), offset, Type.INT);
+            return Type.INT;
+        }
+
+        return null;
     }
 
-    private static String formatString(String s, int offset, boolean isObject) {
-        return (isObject ? "\u001B[34mOBJ" : "\u001B[32mSTR") + "[" + offset + " | 0x" + Integer.toHexString(offset) + "]\u001B[0m " + s;
+    private static Type printObjName(long address, int offset) {
+        String result = API.readMemoryStringFallback(address, null);
+
+        if (result != null && !result.isEmpty()) {
+            printStr(replacements.getOrDefault(result, result), offset, Type.OBJ);
+            return Type.OBJ;
+        }
+        return null;
+    }
+
+    private static void printStr(String s, int offset, Type type) {
+        System.out.println(type + "[" + offset + " | 0x" + Integer.toHexString(offset) + "]\u001B[0m " + s);
     }
 
     private static void updateReplacements() {
