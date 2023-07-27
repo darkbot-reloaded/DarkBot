992d78ca7602f9c8e14f8d078c8bd3176dab7a2f
dm94 <dm94@hotmail.es>
2023-07-27 20:25:19 +0200

    Updated quest proxy

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/QuestProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/QuestProxy.java
@@ -1,19 +1,16 @@
 package com.github.manolo8.darkbot.core.objects.facades;
 
 import com.github.manolo8.darkbot.core.itf.Updatable;
-import com.github.manolo8.darkbot.core.objects.swf.ObjArray;
 import com.github.manolo8.darkbot.core.utils.ByteUtils;
 
 import eu.darkbot.api.API;
 
 import static com.github.manolo8.darkbot.Main.API;
 
-public class QuestProxy extends Updatable implements API.Singleton {
+import org.jetbrains.annotations.Nullable;
 
-    private final ObjArray questConditionsArr = ObjArray.ofVector(true);
-    private boolean active;
-    private String category, title, descriptiuon;
-    private int id;
+public class QuestProxy extends Updatable implements API.Singleton {
+    private Quest currentQuest = new Quest();
 
     @Override
     public void update() {
@@ -23,13 +20,61 @@ public class QuestProxy extends Updatable implements API.Singleton {
 
         long questClass = API.readMemoryLong(address + 0x98) & ByteUtils.ATOM_MASK;
 
-        long actualQuest = API.readMemoryLong(questClass + 0x28) & ByteUtils.ATOM_MASK;
+        long currentQuestAddr = API.readMemoryLong(questClass + 0x28) & ByteUtils.ATOM_MASK;
 
-        this.id = API.readMemoryInt(actualQuest + 0x20);
-        this.active = API.readMemoryBoolean(actualQuest + 0x24);
-        this.category = API.readMemoryString(actualQuest, 0x48);
-        this.title = API.readMemoryString(actualQuest, 0x68);
-        this.descriptiuon = API.readMemoryString(actualQuest, 0x70);
+        if (currentQuestAddr == 0) {
+            this.currentQuest = null;
+        } else {
+            if (this.currentQuest == null) {
+                this.currentQuest = new Quest();
+            }
+
+            this.currentQuest.update(currentQuestAddr);
+        }
     }
 
+    public @Nullable Quest getCurrenQuest() {
+        return currentQuest;
+    }
+
+    public static class Quest extends Auto {
+        private boolean active;
+        private String category;
+        private String description;
+        private String title;
+        private int id;
+
+        @Override
+        public void update() {
+            if (address == 0) {
+                return;
+            }
+
+            this.id = API.readMemoryInt(address + 0x20);
+            this.active = API.readMemoryBoolean(address + 0x24);
+            this.category = API.readMemoryString(address, 0x48);
+            this.title = API.readMemoryString(address, 0x68);
+            this.description = API.readMemoryString(address, 0x70);
+        }
+
+        public int getCurrentQuestId() {
+            return id;
+        }
+
+        public boolean isActive() {
+            return active;
+        }
+
+        public String getCategory() {
+            return category;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDescriptiuon() {
+            return description;
+        }
+    }
 }
