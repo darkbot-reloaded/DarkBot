62664d1eb6134dec6c7de08aacd2697ccfc8383e
dm94 <dm94@hotmail.es>
2023-07-28 18:14:41 +0200

    Updated version to commit 2e872bb203

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ publishing {
 }
 
 dependencies {
-    val apiVersion = "0.7.7"
+    val apiVersion = "2e872bb203"
     val flatLafVersion = "3.1.1"
 
     // use this if you want to use local(mavenLocal) darkbot API
