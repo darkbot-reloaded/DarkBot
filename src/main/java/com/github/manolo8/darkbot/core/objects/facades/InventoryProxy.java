e052cea93dee1efc0ede359ca0d86909fef63a64
Anjesh Shrestha <anjeshshrestha@gmail.com>
2023-09-21 16:56:17 -0600

    inventory proxy, timer every 10 seconds

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/InventoryProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/InventoryProxy.java
@@ -4,6 +4,7 @@ import com.github.manolo8.darkbot.Main;
 import com.github.manolo8.darkbot.core.itf.Updatable;
 import com.github.manolo8.darkbot.core.objects.swf.ObjArray;
 import eu.darkbot.api.managers.InventoryAPI;
+import eu.darkbot.util.Timer;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.ToString;
@@ -19,16 +20,19 @@ public class InventoryProxy extends Updatable implements InventoryAPI {
     private final ObjArray itemsArr = ObjArray.ofVector(true);
 
     public List<Item> items = new ArrayList<>();
+    private final Timer updateTimer = Timer.get(10_000);
 
     @Override
     public void update() {
-        itemsArr.update(Main.API.readMemoryPtr(address, 0x30, 0x58));
-        itemsArr.sync(items, Item::new);
+        if (updateTimer.tryActivate()) {
+            itemsArr.update(Main.API.readMemoryPtr(address, 0x30, 0x58));
+            itemsArr.syncAndReport(items, Item::new);
+        }
     }
 
     @Getter
     @ToString
-    private static class Item extends Auto implements InventoryAPI.Item {
+    private static class Item extends Reporting implements InventoryAPI.Item {
         private String lootId = "";
         private String name = "";
         private double amount;
@@ -38,6 +42,19 @@ public class InventoryProxy extends Updatable implements InventoryAPI {
             lootId = API.readMemoryString(address, 0x48);
             name = API.readMemoryString(address, 0x40, 0x20, 0x40);
             amount = API.readDouble(address + 0x70);
+        }
+
+        @Override
+        public boolean updateAndReport() {
+            if (address == 0) return false;
+
+            double amount = API.readDouble(address + 0x70);
+            if (this.amount == amount) return false;
+            this.amount = amount;
+
+            this.lootId = API.readMemoryString(address, 0x48);
+            this.name = API.readMemoryString(address, 0x40, 0x20, 0x40);
+            return true;
         }
     }
 }
