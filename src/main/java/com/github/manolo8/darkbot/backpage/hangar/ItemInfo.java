e1fe91534624b984b713ab64b7665171d21474d7
m9w <52008918+m9w@users.noreply.github.com>
2024-02-21 23:05:19 +0200

    Fix parsing PET gears

--- src/main/java/com/github/manolo8/darkbot/backpage/hangar/ItemInfo.java
+++ src/main/java/com/github/manolo8/darkbot/backpage/hangar/ItemInfo.java
@@ -1,19 +1,27 @@
 package com.github.manolo8.darkbot.backpage.hangar;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import com.google.gson.annotations.JsonAdapter;
 import com.google.gson.annotations.SerializedName;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.net.URL;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ItemInfo extends Item {
     private String name;
     private String localizationId;
     @SerializedName("C") private String category;
-    private List<Map<String, Object>> levels;
+    @JsonAdapter(LevelsDeserializer.class) private List<Map<String, Object>> levels;
 
     public String getName() {
         return name;
@@ -67,5 +75,21 @@ public class ItemInfo extends Item {
                 ", category='" + category + '\'' +
                 ", levels=" + levels +
                 "} " + super.toString();
+    }
+
+    private static class LevelsDeserializer implements JsonDeserializer<List<Map<String, Object>>> {
+        @Override
+        public List<Map<String, Object>> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+            if (json.isJsonArray()) return context.deserialize(json, typeOfT);
+            if (json.isJsonObject()) {
+                final JsonObject obj = json.getAsJsonObject();
+                return obj.keySet().stream().map(key -> {
+                    Map<String, Object> map = context.deserialize(obj.get(key), Map.class);
+                    map.put("level", key);
+                    return  map;
+                }).collect(Collectors.toList());
+            }
+            return null;
+        }
     }
 }
