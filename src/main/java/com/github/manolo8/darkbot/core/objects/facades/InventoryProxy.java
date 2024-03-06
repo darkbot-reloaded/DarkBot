bf10b3b2f3fb0280bcf74ea344b3be5bd963c72e
Anjesh Shrestha <anjeshnp@amazon.com>
2024-03-06 12:58:22 -0800

    use flashlist & timer

--- src/main/java/com/github/manolo8/darkbot/core/objects/facades/InventoryProxy.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/facades/InventoryProxy.java
@@ -2,14 +2,13 @@ package com.github.manolo8.darkbot.core.objects.facades;
 
 import com.github.manolo8.darkbot.Main;
 import com.github.manolo8.darkbot.core.itf.Updatable;
-import com.github.manolo8.darkbot.core.objects.swf.ObjArray;
+import com.github.manolo8.darkbot.core.objects.swf.FlashList;
 import eu.darkbot.api.managers.InventoryAPI;
 import eu.darkbot.util.Timer;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.ToString;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static com.github.manolo8.darkbot.Main.API;
@@ -17,44 +16,38 @@ import static com.github.manolo8.darkbot.Main.API;
 @Getter
 public class InventoryProxy extends Updatable implements InventoryAPI {
     @Getter(AccessLevel.NONE)
-    private final ObjArray itemsArr = ObjArray.ofVector(true);
-
-    public List<Item> items = new ArrayList<>();
+    public FlashList<Item> items = FlashList.ofVector(Item::new);
     private final Timer updateTimer = Timer.get(10_000);
 
     @Override
     public void update() {
         if (updateTimer.tryActivate()) {
-            itemsArr.update(Main.API.readMemoryPtr(address, 0x30, 0x58));
-            itemsArr.syncAndReport(items, Item::new);
+            items.update(Main.API.readAtom(address, 0x30, 0x58));
         }
+    }
+
+    @Override
+    public List<? extends InventoryAPI.Item> getItems(int minWaitMs) {
+        long elapsedMs = 10_000L - updateTimer.getRemainingFuse();
+        if (elapsedMs > minWaitMs) {
+            updateTimer.tryDisarm();
+        }
+        update();
+        return items;
     }
 
     @Getter
     @ToString
-    private static class Item extends Reporting implements InventoryAPI.Item {
+    private static class Item extends Updatable implements InventoryAPI.Item {
         private String lootId = "";
         private String name = "";
         private double amount;
 
         public void update() {
             if (address <= 0) return;
-            lootId = API.readMemoryString(address, 0x48);
-            name = API.readMemoryString(address, 0x40, 0x20, 0x40);
+            lootId = API.readString(address, 0x48);
+            name = API.readString(address, 0x40, 0x20, 0x40);
             amount = API.readDouble(address + 0x70);
-        }
-
-        @Override
-        public boolean updateAndReport() {
-            if (address == 0) return false;
-
-            double amount = API.readDouble(address + 0x70);
-            if (this.amount == amount) return false;
-            this.amount = amount;
-
-            this.lootId = API.readMemoryString(address, 0x48);
-            this.name = API.readMemoryString(address, 0x40, 0x20, 0x40);
-            return true;
         }
     }
 }
