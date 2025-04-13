54ad25b1b4d0ed1ccd82810e9d83ad3301af7a9c
Anjesh Shrestha <anjeshnp@amazon.com>
2025-04-13 01:30:25 -0700

    auto close assembly

--- src/main/java/com/github/manolo8/darkbot/core/objects/gui/AssemblyGui.java
+++ src/main/java/com/github/manolo8/darkbot/core/objects/gui/AssemblyGui.java
@@ -1,4 +1,22 @@
 package com.github.manolo8.darkbot.core.objects.gui;
 
-public class AssemblyGui {
+import com.github.manolo8.darkbot.core.objects.Gui;
+import eu.darkbot.api.API;
+import eu.darkbot.api.managers.BotAPI;
+import eu.darkbot.util.Timer;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AssemblyGui extends Gui implements API.Singleton {
+    private final BotAPI bot;
+    private final Timer guiUsed = Timer.getRandom(19_000, 1000);
+
+    @Override
+    public void update() {
+        super.update();
+        // Last gui usage >20s ago, close gui
+        if (bot.isRunning() && guiUsed.isInactive()) {
+            this.show(false);
+        }
+    }
 }
